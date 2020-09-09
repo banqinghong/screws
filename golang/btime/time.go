026be71801f82c50ba974e420514bd13406a2af9
李杨 <liyang@liyangdeMacBook-Pro.local>
2020-09-09 11:41:36 +0800

    update

--- golang/btime/time.go
+++ golang/btime/time.go
@@ -4,11 +4,17 @@ import (
 	"time"
 )
 
-// 时间戳转换为给定的格式，如果给定模板为空，则转换为 "2006-01-02 15:04:05"
-func FormatTimestamp(ts int64, timeTemplate string)(timeString string){
+// 时间戳转换为给定的格式，
+// 如果给定模板为空，则转换为 "2006-01-02 15:04:05"
+// 如果给定时间戳为0，则默认使用当前时间
+func FormatTimestamp(timestamp int64, timeTemplate string)(timeString string){
 	tt := "2006-01-02 15:04:05"
 	if timeTemplate != "" {
 		tt = timeTemplate
+	}
+	ts := time.Now().Unix()
+	if timestamp != 0 {
+		ts = timestamp
 	}
 	timeString = time.Unix(ts, 0).Format(tt)
 	return timeString
