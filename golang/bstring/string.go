de2b4c946bf96f8e6f1dc9fbd9f0d9b06defb453
liyang <liyang@codoon.com>
2020-12-18 14:00:58 +0800

    update

--- golang/bstring/string.go
+++ golang/bstring/string.go
@@ -1,7 +1,13 @@
 package bstring
 
 // 判断切片中某个元素是否存在
-func IsExistString (i string, s []string) (res bool){
+func IsExistString(i string, s []string) bool {
+	if len(s) == 0 {
+		return false
+	}
+	if i == "" {
+		return true
+	}
 	for _, v := range s {
 		if v == i {
 			return true
