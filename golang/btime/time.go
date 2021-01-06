acd02d3bb408280f027b5c18f09eefa93544297b
liyang <liyang@codoon.com>
2021-01-06 10:38:15 +0800

    时间字符串串转换为时间

--- golang/btime/time.go
+++ golang/btime/time.go
@@ -7,7 +7,7 @@ import (
 // 时间戳转换为给定的格式，
 // 如果给定模板为空，则转换为 "2006-01-02 15:04:05"
 // 如果给定时间戳为0，则默认使用当前时间
-func FormatTimestamp(timestamp int64, timeTemplate string)(timeString string){
+func FormatTimestamp(timestamp int64, timeTemplate string) (timeString string) {
 	tt := "2006-01-02 15:04:05"
 	if timeTemplate != "" {
 		tt = timeTemplate
@@ -18,4 +18,10 @@ func FormatTimestamp(timestamp int64, timeTemplate string)(timeString string){
 	}
 	timeString = time.Unix(ts, 0).Format(tt)
 	return timeString
+}
+
+func String2Time(timeString string) (time.Time, error) {
+	tt := "2006-01-02 15:04:05"
+	st, err := time.Parse(tt, timeString)
+	return st, err
 }
