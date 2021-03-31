f84bf3dcd6a13c7b7aaa70df7e7ab94f7376ff85
liyang <liyang@codoon.com>
2021-03-31 16:01:33 +0800

    update

--- golang/btime/time.go
+++ golang/btime/time.go
@@ -26,12 +26,23 @@ func String2Time(timeString string) (time.Time, error) {
 	return st, err
 }
 
-// 获取当前时刻的n月前第一天
-func AddDate(t time.Time, years int, months int) time.Time {
+// 获取当前时刻的n月前第一天的当前时刻
+func FirstDayOfMonthAfterAddDate(t time.Time, years int, months int) time.Time {
 	year, month, _ := t.Date()
 	hour, min, sec := t.Clock()
 	// AddDate后月份的第一天
 	firstDayOfMonthAfterAddDate := time.Date(year+years, month+time.Month(months), 1,
 		hour, min, sec, t.Nanosecond(), t.Location())
 	return firstDayOfMonthAfterAddDate
+}
+
+// 获取当前时刻的n月前最后一天
+func LastDayOfMonthAfterAddDate(t time.Time, years int, months int) time.Time {
+	year, month, _ := t.Date()
+	// AddDate后月份的第一天
+	firstDayOfMonthAfterAddDate := time.Date(year+years, month+time.Month(months), 1,
+		0, 0, 0, 0, t.Location())
+	// AddDate后月份的最后一个时刻
+	lastDayOfMonthAfterAddDate := firstDayOfMonthAfterAddDate.AddDate(0, 1, 0).Add(-time.Nanosecond)
+	return lastDayOfMonthAfterAddDate
 }
